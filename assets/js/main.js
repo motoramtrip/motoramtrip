18d4625a5d4d03763c324bd0313c01fd780efac7
diogo1pires <45296613+diogo1pires@users.noreply.github.com>
2019-12-31 11:55:14 +0000

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,13 @@
 //Animate smooth scroll - Little help by Miguel Cosme => https://github.com/MiguelCosme 
 function scrollDown(elementId) { 
-    const y = document.getElementById(elementId).offsetTop;
+    
+	var obj = document.getElementById(elementId);
+	
+	const ot = obj.offsetTop;
+	const p  = parseInt(window.getComputedStyle(obj).padding);
+	
+	// offsetTop - padding
+	const y = ot - p;
+	
     window.scroll({ top: y, behavior: 'smooth' });
 }
